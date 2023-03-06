8ce5f28e88d6f112eab7b0af126ff508cd2e29f5
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-03-06 13:08:42 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 radio.setGroup(174)
 basic.forever(function () {
-	
+    if (KSB045.getBtnValue(KSB045.btnName.E)) {
+        radio.sendNumber(1)
+    }
+    if (KSB045.getBtnValue(KSB045.btnName.C)) {
+        radio.sendNumber(2)
+    }
 })
